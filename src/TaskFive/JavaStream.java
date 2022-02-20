e6c0f01c4d0c8dafbfb18048d06095dab07e49ef
VladMoko <advmoney2233@gmail.com>
2022-02-20 13:41:56 +0300

    update

--- src/TaskFive/JavaStream.java
+++ src/TaskFive/JavaStream.java
@@ -7,17 +7,17 @@ import java.util.stream.Stream;
 
 public class JavaStream {
     public static void main(String[] args) {
-        Stream<String> streamA = Stream.of("Alfa", "Beta", "Gama");
-        Stream<String> streamB = Stream.of("1", "2", "3", "4");
+        Stream<String> first = Stream.of("Alfa", "Beta", "Gama");
+        Stream<String> second = Stream.of("1", "2", "3", "4");
 
-        Stream<String> result = zip(streamA, streamB);
+        Stream<String> result = zip(first, second);
         result.peek(System.out::println).collect(Collectors.toList());
     }
 
 
-    public static <T> Stream<T> zip(Stream<T> streamA, Stream<T> streamB) {
-        List<T> collect = streamA.collect(Collectors.toList());
-        List<T> collect1 = streamB.collect(Collectors.toList());
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        List<T> collect = first.collect(Collectors.toList());
+        List<T> collect1 = second.collect(Collectors.toList());
 
         int min = Math.min(collect.size(), collect1.size());
         List<T> result = new ArrayList<>();
